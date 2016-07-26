e079ac797849c9f0022d712724485eb272385633
Alex Lawrence <mail@alex-lawrence.com>
2016-07-26 15:52:45 +0200

    Switch from Janus v0.1.1 to refcount branch

--- janus.sh
+++ janus.sh
@@ -1,6 +1,6 @@
 cd ~
 git clone https://github.com/meetecho/janus-gateway.git
-git checkout v0.1.1
+git checkout refcount
 cd janus-gateway
 sh autogen.sh
 ./configure --prefix=/opt/janus --disable-rabbitmq
