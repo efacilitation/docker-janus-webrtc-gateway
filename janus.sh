be56c700af9d3bd517e06cfa7c8d9dd908555a17
Ferdinand Full <reg@medialwerk.de>
2016-09-26 12:53:08 +0200

    Update janus gateway to v.0.1.2

--- janus.sh
+++ janus.sh
@@ -1,6 +1,6 @@
 cd ~
 git clone https://github.com/meetecho/janus-gateway.git
-git checkout refcount
+git checkout v0.1.2
 cd janus-gateway
 sh autogen.sh
 ./configure --prefix=/opt/janus --disable-rabbitmq
