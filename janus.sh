d4d2c5033fb9048d1245edab1332b6a640bfea34
Ferdinand Full <reg@medialwerk.de>
2016-10-20 14:58:20 +0200

    Add --disable-mqtt to janus.sh

--- janus.sh
+++ janus.sh
@@ -3,7 +3,7 @@ git clone https://github.com/meetecho/janus-gateway.git
 git checkout v0.2.0
 cd janus-gateway
 sh autogen.sh
-./configure --prefix=/opt/janus --disable-rabbitmq
+./configure --prefix=/opt/janus --disable-rabbitmq --disable-mqtt
 make
 make install
 make configs
