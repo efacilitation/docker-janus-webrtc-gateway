765a2425ff40326a420eb8618ef50f68c0c656bb
Ferdinand Full <reg@medialwerk.de>
2016-12-05 17:50:03 +0100

    [SIXSTEPS-3962] Remove janus v0.2.0 checkout command

--- janus.sh
+++ janus.sh
@@ -1,7 +1,6 @@
 cd ~
 git clone https://github.com/meetecho/janus-gateway.git
 cd janus-gateway
-git checkout v0.2.0
 sh autogen.sh
 ./configure --prefix=/opt/janus --disable-rabbitmq --disable-mqtt
 make
