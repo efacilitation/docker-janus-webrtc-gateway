10ee54c08d23dd46d140a542138581f6c83c51ee
Ferdinand Full <reg@medialwerk.de>
2016-12-05 17:50:32 +0100

    Revert "[SIXSTEPS-3962] Remove janus v0.2.0 checkout command"
    This reverts commit 765a2425ff40326a420eb8618ef50f68c0c656bb.

--- janus.sh
+++ janus.sh
@@ -1,6 +1,7 @@
 cd ~
 git clone https://github.com/meetecho/janus-gateway.git
 cd janus-gateway
+git checkout v0.2.0
 sh autogen.sh
 ./configure --prefix=/opt/janus --disable-rabbitmq --disable-mqtt
 make
