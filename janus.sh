0e93415e502fd76ae4c7fac7239283dfd8db3286
Ferdinand Full <reg@medialwerk.de>
2017-03-16 11:53:20 +0100

    [SIXSTEPS-4152] Add janus docs configuration

--- janus.sh
+++ janus.sh
@@ -3,7 +3,7 @@ git clone https://github.com/meetecho/janus-gateway.git
 cd janus-gateway
 git checkout v0.2.2
 sh autogen.sh
-./configure --prefix=/opt/janus --disable-rabbitmq --disable-mqtt
+./configure --prefix=/opt/janus --disable-rabbitmq --disable-mqtt --enable-docs
 make
 make install
 make configs
