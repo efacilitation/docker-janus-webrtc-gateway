dbe110be384fa6e8e59c9e03bcc859f15ffb2489
Ferdinand Full <reg@medialwerk.de>
2017-03-16 11:54:18 +0100

    [SIXSTEPS-4152] Remove janus version checkout (use latest janus version on master)

--- janus.sh
+++ janus.sh
@@ -1,7 +1,6 @@
 cd ~
 git clone https://github.com/meetecho/janus-gateway.git
 cd janus-gateway
-git checkout v0.2.2
 sh autogen.sh
 ./configure --prefix=/opt/janus --disable-rabbitmq --disable-mqtt --enable-docs
 make
