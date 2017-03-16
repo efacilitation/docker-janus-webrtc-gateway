0f3fc1d631eade419847f7cacacf2ca12782e33d
Ferdinand Full <reg@medialwerk.de>
2017-03-16 12:00:49 +0100

    [SIXSTEPS-4152] Add janus version 0.2.2 checkout

--- janus.sh
+++ janus.sh
@@ -1,6 +1,7 @@
 cd ~
 git clone https://github.com/meetecho/janus-gateway.git
 cd janus-gateway
+git checkout v0.2.2
 sh autogen.sh
 ./configure --prefix=/opt/janus --disable-rabbitmq --disable-mqtt --enable-docs
 make
