75c35fa717e913179bd86e66828d2b0559e2bf8f
galarus <dwmarx@ucsc.edu>
2018-05-13 00:34:03 -0700

    Update janus.sh
    update to fix errors caused by configuring new version of Janus without lua

--- janus.sh
+++ janus.sh
@@ -2,7 +2,7 @@ cd ~
 git clone https://github.com/meetecho/janus-gateway.git
 cd janus-gateway
 sh autogen.sh
-./configure --prefix=/opt/janus --disable-rabbitmq --disable-mqtt --enable-docs
+./configure --prefix=/opt/janus --disable-rabbitmq --disable-mqtt --disable-plugin-lua --enable-docs
 make
 make install
 make configs
